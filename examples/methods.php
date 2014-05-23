61593709e2eeea94ed8c2f3c1662664991eecae0
rbarros <ricardofbarros@hotmail.com>
2014-05-23 09:50:14 +0100

    Add examples

---
+++ examples/methods.php
@@ -0,0 +1,42 @@
+<?php
+include 'autoload_examples.php';
+
+/**
+ * Class A extends to ClassB, ClassC
+ */
+class ClassA extends \Inheritance{
+    
+    public function __construct() {
+        parent::__inherit(array(
+            'ClassB', 
+            'ClassC'
+         ));
+    }
+    
+    public function test() {
+       return parent::hello().' '.parent::world();
+    }
+}
+
+/**
+ * Class B
+ */
+class ClassB  {
+    protected function hello() {
+        return 'Hello';
+    }
+}
+
+/**
+ * Class C
+ */
+class ClassC {
+    protected function world() {
+        return 'World!';
+    }
+}
+
+$class = new ClassA();
+
+// Expected Output : Hello World!
+echo $class->test();
