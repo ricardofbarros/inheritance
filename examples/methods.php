fcfa98b032077f77528906521e22e123f24b3837
rbarros <ricardofbarros@hotmail.com>
2014-05-23 12:03:53 +0100

    update examples

--- examples/methods.php
+++ examples/methods.php
@@ -1,5 +1,15 @@
 <?php
-include 'autoload_examples.php';
+define('DS', DIRECTORY_SEPARATOR);
+include '..'.DS.'src'.DS.'Inheritance.php';
+
+// Register auto loader
+Inheritance::registerAutoloader();
+
+#############################
+# 
+# Example 1 - Extending Class
+#
+#############################
 
 /**
  * Class A extends to ClassB, ClassC
@@ -22,6 +32,10 @@ class ClassA extends \Inheritance{
  * Class B
  */
 class ClassB  {
+    public function helloWorld() {
+        return 'This is a public Hello World!';
+    }
+    
     protected function hello() {
         return 'Hello';
     }
@@ -40,3 +54,56 @@ $class = new ClassA();
 
 // Expected Output : Hello World!
 echo $class->test();
+
+// Expected Output : This is a public Hello World!
+echo $class->helloWorld();
+
+
+########################################
+# 
+# Example 2 - Trying to access to 
+# protected methods from an invalid scope
+#
+#########################################
+
+// ClassA - Throws Exception
+echo $class->hello().' '.$class->world();
+
+
+/**
+ * Class Invalid
+ */
+class ClassInvalid {
+    public function test() {
+        $class = new ClassA();
+        return $class->hello().' '.$class->world(); 
+    } 
+}
+
+$classInvalid = new ClassInvalid();
+
+// Throw exception
+echo $classInvalid->test();
+
+
+
+########################################
+# 
+# Example 3 - Grandchild inherit methods
+#  from multiple grandparents
+# 
+#########################################
+
+/**
+ * Class Grand Child
+ */
+class ClassGrandChild extends ClassA {
+    public function testParent() {
+       return parent::hello().' '.parent::world();
+    } 
+}
+
+$classGrandChild = new ClassGrandChild();
+
+// Expected Output : Hello World!
+echo $classGrandChild->testParent();
